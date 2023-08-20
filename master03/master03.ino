fb567965b73fe22feb863a2e78cbaddf17d7fa91
Max Landeo <maxlr2005@gmail.com>
2023-08-19 21:55:13 -0500

    update slave03 success

--- master03/master03.ino
+++ master03/master03.ino
@@ -1,123 +1,143 @@
-// * CODIGO MASTER 06
+#include <AccelStepper.h> 
 #include <Wire.h>
 
-const int I2C_SLAVE_ADDR_1 = 8;  //Direccion del esclavo1 MANO DER
-const int I2C_SLAVE_ADDR_2 = 9;  //Direccion del esclavo2 PIE DER
-const int I2C_SLAVE_ADDR_3 = 10; //Direccion del esclavo3 PIE IZQ
-const int I2C_SLAVE_ADDR_4 = 11; //Direccion del esclavo4 MANO IZQ
-const int I2C_SLAVE_ADDR_5 = 12; //Direccion del esclavo5 CUERPO
+// Configuracion del stepper
+AccelStepper stepperX(1, 5, 4);  
 
-// * SECUENCIA DE BAILE
-// Tiempo de baile 3min
-// tiempo aprox por paso 6s
-// total de pasos resultante 30
-// cada paso se representa como un int con valores entre -127 y 127
-// donde 0 es la posicion inicial del hilo, -127 es la mas alta y 127 la mas baja
-const int motores=5; //cantidad de motores, constante necesaria para deconstruir los pasos luego
-// para demostracion pasos 16
-const int pasos= 16; //constante necesaria para deconstruir los pasos luego
-int baile [pasos][motores] = {
-    //test manos
-    {-30,0,0,-30,0},
-    {0,0,0,0,0},
-    //test piernas
-    {0,-30,-30,0,0},
-    {0,0,0,0,0},
-    //test cuerpo
-    {0,0,0,0,-30},
-    {0,0,0,0,0},
-    //la ola
-    {-20,0,0,0,0},
-    {-40,-20,0,0,0},
-    {-60,-40,0,0,-20},
-    {-40,-60,-20,0,-40},
-    {-20,-40,-40,-20,-60},
-    {0,-20,-60,-40,-40},
-    {0,0,-40,-60,-20},
-    {0,0,-20,-40,0},
-    {0,0,0,-20,0},
-    {0,0,0,0,0},
-    };
+#define home_switch 12 // Define Pin 12 connectado como Switch FIN DE CARRERA (MicroSwitch)
 
-    
+//*Variables para el stepper
 
-//* VOID DE ORDENES
-void mandarPasos(const int a[][motores]){
-    int ok = 0;
-    int done=0;
-    for (int i = 0; i < pasos; i++)//revisa la matriz de baile paso por paso
-    {
-       for (int j = 0; j < motores; j++) //revisa el paso motor por motor
-       {
-        Serial.print("Valor a enviar: ");
-        Serial.print(a[i][j]);
-        Wire.beginTransmission(j+8);//inicia transmision al esclavo reemplazando la direcion con la variable
-        int pass = a[i][j]+127; //reemplaza el valor del baile agregando 127 para transmitir el byte. 
-        Wire.write(pass);//escribe los valores para mandar al buffer
-        Wire.endTransmission();//transmite los valores
-       };
+long initial_homing=-1;  // Usado para setear el final de carrera
+long posicioncero = 2500;  //* DEFINE LA POSICION INICIAL CON RESPECTO AL TOPE siempre en positivo
+long margen = 200;// Margen de seguridad para que el punto mas alto no toque el final de carrera
+long MAX_up= margen - posicioncero;
+long MAX_down= posicioncero;
 
-       //* CONFIRMACION
-       for (int y = 0; y < motores; y++)//revisa confirmacion motor por motor
-       {
-        ok = 0;//Reset ok
-        done=0;//Reset done
-        while (done != 1)
-        {
-        Wire.requestFrom(y+8,1);//solicita confirmacion por parte de los slaves como 1 byte
-        while (Wire.available())//espera disponibilidad de respuesta
-        {
-            ok = Wire.read(); //reemplaza el valor de lectura en la variable ok
-            Serial.println("recibiendo..."); //notificacion en serial[DEV]
-            Serial.print(ok);//debe ser 2
-        };
-        if (ok==2) //corroboracion de respuesta = 2
-            {
-            Serial.println("motor"+String(y+1)+"is done"); //confirmacion a serial [DEV]
-            done=1; //rompe el while de "done" para continuar al siguiente motor
-            };
+const double v_set=250.0; //VELOCIDAD DE SETEO
+const double a_set=100.0; //ACELERACION DE SETEO
 
-        delay(200);//Espera para volver a solicitar
-        }
-       };
-    };
-    
+const double vel=1200.0; //VELOCIDAD EN FUNCIONAMIENTO
+const double acel=7000.0; //ACELERACION EN FUNCIONAMIENTO
+long stop; 
+long steps;
+//*Variables para comunicacion I2C
+const int i2c_address=12; // Direccion del slave en i2c (8=1, 9=2, 10=3, 11=4. 12=5)
+int listo=0; //variable contador de confirmacion
+int bits=1; //pta funciona pe
+int paso;
+
+//SETUP
+void setup() {
+   Serial.begin(9600);  // Inicia monitor serial [DEV]
+   
+   pinMode(home_switch, INPUT_PULLUP); //Configuracion pin FIN DE CARRERA
+   
+   delay(1000);  // *tiempo de espera para encender drivers y liberar reles
+
+   //  Configuracion inicial para seteo
+  stepperX.setMaxSpeed(v_set);      // Set Max Speed of Stepper (Slower to get better accuracy)
+  stepperX.setAcceleration(a_set);  // Set Acceleration of Stepper
+ 
+
+// Start Homing procedure of Stepper Motor at startup
+
+ // Serial.print("Stepper is Homing . . . . . . . . . . . "); //notificacion a serial [DEV]
+
+  while (digitalRead(home_switch)) {  // Make the Stepper move CCW until the switch is activated   
+    stepperX.moveTo(initial_homing);  // Set the position to move to
+    initial_homing--;  // Decrease by 1 for next move if needed
+    stepperX.run();  // Start moving the stepper
+    delay(4);
 }
 
-//* SETUP
-void setup(){
-    Wire.begin();
-    Serial.begin(9600); //inicia monitor serial [DEV]
-    int listo = 0;
-    for (int i = 0; i < motores; i++) //inicia verificacion para cada motor
-    {
-        while (listo != 1)
+  stepperX.setCurrentPosition(0);  // Set the current position as zero for now
+  stepperX.setMaxSpeed(v_set);      // Velocidad de seteo
+  stepperX.setAcceleration(a_set);  // Aceleracion seteo
+  initial_homing=1;
+
+  while (!digitalRead(home_switch)) { // Make the Stepper move CW until the switch is deactivated
+    stepperX.moveTo(initial_homing);  
+    stepperX.run();
+    initial_homing++;
+    delay(4);
+  }
+  //SECUENCIA DE SETEO A POSICION CERO
+  stepperX.setCurrentPosition(0);
+  stepperX.runToNewPosition(posicioncero);
+  stepperX.setCurrentPosition(0);
+ // Serial.println("Homing Completed"); //Notificacion a serial [DEV]
+  stepperX.setMaxSpeed(vel);      // Restauracion a velocidad normal
+  stepperX.setAcceleration(acel);  // Restauracion a aceleracion normal
+
+  //COMUNICACION i2C
+  Wire.begin(i2c_address); //inicia comunicacion en i2c como slave
+  Wire.onRequest(LaunchOk);
+  Wire.onReceive(Master_says);
+}
+
+
+//*FUNCION DE CONFIRMACION
+void LaunchOk(){
+  while (listo==0)
+  {
+    listo=1;
+    Wire.write(1);
+  //  Serial.println("Launch enviado!");
+  }
+  if (listo==1)
+      {
+        delay(1000);//tiempo para que reaccione el stepper [PATCH]
+        if (stepperX.currentPosition()!= steps)
         {
-            int address_slave = i+8;
-            Wire.requestFrom(address_slave,1);
-            delay(500);//espera a que responda el slave
-            while (Wire.available() >= 1)
-            {
-                listo = int(Wire.read());
-                Serial.print("valor recibido: ");
-                Serial.println(listo);
-            }
-            delay(500); //Tiempo de espera para volver a solicitar
+          Wire.write(3);// porsiacaso 3 para no confundir con 1 de launch
+         // Serial.println("aun no esta listo");
         }
+        else
+        {
+          Wire.write(2);
+         // Serial.println("listo enviado");
+        }
+      }
+}
+void Master_says (int numBytes){
+  paso = Wire.read();
+ Serial.println(paso);
 
-        Serial.println("Motor"); //NOTIFICACION A SERIAL [DEV]
-        Serial.print(i+1);
-        Serial.print("listo!");
+  // Serial.println("Recibido: ");
+ // Serial.print(paso);
+  steps = map(long(paso), -127,127,MAX_up,MAX_down);
+ 
+ // Serial.print(" transforma");
+  //Serial.print(steps);
+  delay(5);//delay para que el esclavo escriba los valores en el serial
+  
+  
+}
 
-        listo = 0; //reset de variable listo
+//*LOOP
+void loop() {
+   if (paso== 255){ //666 en paso + 127
+  stepperX.setMaxSpeed(1000);      // Set Max Speed of Stepper (Slower to get better accuracy)
+  stepperX.setAcceleration(250);  // Set Acceleration of Stepper
+ 
+    stepperX.moveTo(15000);
+     while (stepperX.distanceToGo() != 0)
+  {
+    stepperX.run();
+  }
+    Serial.println("Chau ");
+    while (true) {
+      // Bucle infinito, el programa se quedará estancado aquí
+      // hasta que reinicies manualmente el Arduino
     }
-    Serial.println("TODOS LOS MOTORES LISTOS!");
-    delay(3000); //Cuenta regresiva 3..2..1
-} 
-
-
-//* LOOP
-void loop(){
-    mandarPasos(baile);
-    delay(1800000);//no hace nada por media hora
+  }  
+  stepperX.moveTo(steps);
+  //stop = steps - long(acel);
+  while (stepperX.distanceToGo() != 0)
+  {
+    stepperX.run();
+  }
+ // steps = 0;
+  delay(5); //porsiacas
 }
